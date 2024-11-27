698f8b6e08d577c8882026a476ad40c25c9a1a1c
Maksim Ustsinovich <maksimustsinovich@gmail.com>
2024-11-27 12:33:50 +0300

    initial commit

---
+++ src/main/java/by/ustsinovich/stompchat/StompChatApplication.java
@@ -0,0 +1,13 @@
+package by.ustsinovich.stompchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StompChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StompChatApplication.class, args);
+    }
+
+}
